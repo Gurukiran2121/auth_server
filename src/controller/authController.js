d3dd0694d536f8bc7ef627dc894d1537d8932295
Gurukiran SN <gurukiran.sn@netenrich.com>
2025-05-26 12:20:40 +0530

    updated ath controler

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,10 +1,36 @@
+import prisma from "../utils/prismaClient.js";
+
 const authController = async (req, resp) => {
-    console.log("authController called");
-    try {
-        console.log(req.body);
-    } catch (error) {
-        console.error(error);
-    }
+  const { email, name, sub, picture, email_verified } = req.body;
+
+  const user = {
+    email,
+    name,
+    auth0Id: sub,
+    picture,
+    emailVerified: email_verified,
+    updatedAt: new Date(),
+  };
+
+  try {
+    const savedUser = await prisma.user.upsert({
+      where: {
+        email: user.email,
+      },
+      update: {
+        name: user.name,
+        picture: user.picture,
+      },
+      create: user,
+    });
+
+    return resp.status(200).json(savedUser);
+  } catch (error) {
+    console.error(error);
+    return resp
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
+  }
 };
 
 export default authController;
